e4043165db79f81dc588305c28861c91fe87d0fd
viktorpykacz <viktor.pykacz@gmail.com>
2024-03-13 13:59:37 +0100

    update rest controllers

---
+++ Controllers/zestawienieGodzinPracyController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyApplication.Data;
+using MyApplication.Models;
+
+namespace MyApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZestawienieGodzinPracyController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ZestawienieGodzinPracyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ZestawienieGodzin
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ZestawienieGodzinPracy>>> GetZestawienieGodzin()
+        {
+            return await _context.ZestawienieGodzinPracy.ToListAsync();
+        }
+
+        //GET: api/ZestawienieGodzin/NazwaProjektu
+        [HttpGet("{NazwaProjektu}")]
+        public async Task<ActionResult<IEnumerable<ZestawienieGodzinPracy>>> GetZestawienieGodzinZProjektu(string NazwaProjektu)
+        {
+            return await _context.ZestawienieGodzinPracy.Where(x => x.NazwaProjektu == NazwaProjektu).ToListAsync();
+        }
+
+        //GET: api/ZestawienieGodzin/Rok/Miesiac
+        [HttpGet("{NazwaProjektu}/{Rok}/{Miesiac}")]
+        public async Task<ActionResult<IEnumerable<ZestawienieGodzinPracy>>> GetZestawienieGodzinZProjektuZRokuZMiesiaca(string NazwaProjektu, int Rok, int Miesiac)
+        {
+            return await _context.ZestawienieGodzinPracy
+                .Where(x => x.NazwaProjektu == NazwaProjektu)
+                .Where(x => x.Rok == Rok)
+                .Where(x => x.Miesiac == Miesiac)
+                .ToListAsync();
+        }
+
+        // POST: api/ZestawienieGodzin
+        [HttpPost]
+        public async Task<ActionResult<ZestawienieGodzinPracy>> PostZestawienieGodzin(ZestawienieGodzinPracy zestawienieGodzin)
+        {
+            _context.ZestawienieGodzinPracy.Add(zestawienieGodzin);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetZestawienieGodzin", new { id = zestawienieGodzin.Id }, zestawienieGodzin);
+        }
+
+        private bool ZestawienieGodzinExists(long id)
+        {
+            return _context.ZestawienieGodzinPracy.Any(e => e.Id == id);
+        }
+
+        // PUT: api/ZestawienieGodzin
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGodziny(int id, ZestawienieGodzinPracy zestawienieGodzin)
+        {
+            if (id != zestawienieGodzin.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(zestawienieGodzin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ZestawienieGodzinExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/ZestawienieGodzin/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteZestawienieGodzin(int id)
+        {
+            var ZestawienieGodzin = await _context.ZestawienieGodzinPracy.FindAsync(id);
+            if (ZestawienieGodzin == null)
+            {
+                return NotFound();
+            }
+
+            _context.ZestawienieGodzinPracy.Remove(ZestawienieGodzin);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
+    }
+
+}
---
+++ Controllers/zestawienieMiesieczneFirmyController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyApplication.Data;
+using MyApplication.Models;
+
+namespace MyApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZestawienieMiesieczneFirmyController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ZestawienieMiesieczneFirmyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ZestawienieMiesieczne
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ZestawienieMiesieczneFirmy>>> GetZestawienieMiesieczne()
+        {
+            return await _context.ZestawienieMiesieczneFirmy.ToListAsync();
+        }
+
+
+        //GET: api/ZestawienieMiesieczne/Rok/Miesiac
+        [HttpGet("{Rok}/{Miesiac}")]
+        public async Task<ActionResult<IEnumerable<ZestawienieMiesieczneFirmy>>> GetZestawienieMiesieczneZRokuZMiesiaca(int Rok, int Miesiac)
+        {
+            return await _context.ZestawienieMiesieczneFirmy
+                .Where(x => x.Rok == Rok)
+                .Where(x => x.Miesiac == Miesiac)
+                .ToListAsync();
+        }
+
+        // POST: api/ZestawienieMiesieczne
+        [HttpPost]
+        public async Task<ActionResult<ZestawienieMiesieczneFirmy>> PostZestawienieMiesieczne(ZestawienieMiesieczneFirmy zestawienieMiesieczne)
+        {
+            _context.ZestawienieMiesieczneFirmy.Add(zestawienieMiesieczne);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetZestawienieMiesieczne", new { id = zestawienieMiesieczne.Id }, zestawienieMiesieczne);
+        }
+
+        private bool ZestawienieMiesieczneExists(long id)
+        {
+            return _context.ZestawienieMiesieczneFirmy.Any(e => e.Id == id);
+        }
+
+        // PUT: api/ZestawienieMiesieczne
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutZestawienieMiesieczne(int id, ZestawienieMiesieczneFirmy zestawienieMiesieczne)
+        {
+            if (id != zestawienieMiesieczne.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(zestawienieMiesieczne).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ZestawienieMiesieczneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/ZestawienieMiesieczne/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteZestawienieMiesieczne(int id)
+        {
+            var ZestawienieMiesieczne = await _context.ZestawienieMiesieczneFirmy.FindAsync(id);
+            if (ZestawienieMiesieczne == null)
+            {
+                return NotFound();
+            }
+
+            _context.ZestawienieMiesieczneFirmy.Remove(ZestawienieMiesieczne);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+    }
+}
